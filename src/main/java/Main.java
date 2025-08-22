8fd7565d83fba0caa4e6ec2381cb8c019e03f4bb
Olga Zotova <olgazotova1976@yandex.ru>
2025-08-22 13:39:37 +0300

    Testone

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        BonusService bonusService = new BonusService();
+        long amount = 1000;
+        boolean registered = true;
+        long bonus = bonusService.calculate(amount, registered);
 
+        System.out.println("Начальная сумма: " + amount);
+        System.out.println("Зарегистрированный: " + registered);
+        System.out.println("Начисленный бонус: " + bonus);
     }
 }
+
